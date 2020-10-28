2272ce22ffd6aefa7259a44a2e6eaa4d93c61826
Emmanuel Paniagua <helaman.paniagua4671@alumnos.udg.mx>
2020-10-28 16:21:25 -0600

    se implemento json.dump en el metodo guardar

--- Libreria_Part/administrador.py
+++ Libreria_Part/administrador.py
@@ -1,4 +1,5 @@
 from .particula import Particula
+import json 
 
 class Administrador:
     def __init__(self):
@@ -21,7 +22,9 @@ class Administrador:
 
     def guardar (self, ubicacion):
         with open(ubicacion, 'w') as archivo:
-            archivo.write(str(self))
+            lista = [particula.to_dict() for particula in self.__particulas]
+            print (lista)
+            json.dump(lista, archivo, indent=5)
 
 #l01 = Particula(id="2306", origen_x="34", origen_y="47", destino_x="90", destino_y="23", velocidad="80", red="90",  green="2", blue="34", distancia="")
 #l02 = Particula(id="2940", origen_x="0", origen_y="0", destino_x="0", destino_y="0", velocidad="0", red="100",  green="200", blue="300", distancia="0")
